663488f10a9fcb6be18abfd5b34d03e23b3f177c
JuliiaHrytsiuk <tuuspoliglotus@gmail.com>
2022-01-17 09:26:57 +0200

    Path fixed

--- src/ContactList/ContactList.js
+++ src/ContactList/ContactList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ContactListField } from "./ContactList.styled";
-import ContactListItem from "./ContactItem";
+import ContactListItem from "../ContactItem";
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
